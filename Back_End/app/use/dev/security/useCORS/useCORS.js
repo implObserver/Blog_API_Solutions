9307cfc925f0b5ae48f1b77e5fcdc7f845489094
implObserver <prognozman@mail.ru>
2024-10-23 21:01:33 +0500

    fix:debug cors

--- Back_End/app/use/dev/security/useCORS/useCORS.js
+++ Back_End/app/use/dev/security/useCORS/useCORS.js
@@ -1,15 +1,29 @@
 import cors from 'cors';
 import { app } from '../../../../app.js';
 
-// Проверка на наличие переменных окружения
-
 export const useCORS = () => {
   app.use((req, res, next) => {
     console.log('Request Origin:', req.headers.origin); // Логируем origin
     console.log('Request Headers:', req.headers); // Логируем заголовки запроса
     console.log(`CORS middleware triggered for origin: ${req.headers.origin}`);
+
+    // Обработка предзапроса (OPTIONS)
+    if (req.method === 'OPTIONS') {
+      res.setHeader('Access-Control-Allow-Origin', req.headers.origin || '*');
+      res.setHeader(
+        'Access-Control-Allow-Methods',
+        'GET, POST, PUT, DELETE, OPTIONS'
+      );
+      res.setHeader(
+        'Access-Control-Allow-Headers',
+        'Content-Type, Authorization'
+      );
+      return res.sendStatus(204); // 204 No Content
+    }
+
     next();
   });
+
   app.use(
     cors({
       origin: [
