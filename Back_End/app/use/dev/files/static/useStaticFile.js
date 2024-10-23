65922da98d97ef74f0ae162bd8bbb8779a661fb5
implObserver <prognozman@mail.ru>
2024-10-23 19:26:45 +0500

    fix:static files

--- Back_End/app/use/dev/files/static/useStaticFile.js
+++ Back_End/app/use/dev/files/static/useStaticFile.js
@@ -4,5 +4,17 @@ import { __dirname } from '../../../../dirname/dirname.js';
 import { app } from '../../../../app.js';
 
 export const useStaticFileServe = () => {
-  app.use(staticFile(join(__dirname, 'public')));
+  app.use(
+    staticFile(join(__dirname, 'public'), {
+      setHeaders: (res, filePath) => {
+        // Устанавливаем правильные MIME-типы для JavaScript и CSS файлов
+        if (filePath.endsWith('.js')) {
+          res.setHeader('Content-Type', 'application/javascript');
+        } else if (filePath.endsWith('.css')) {
+          res.setHeader('Content-Type', 'text/css');
+        }
+        // Здесь можно добавить другие MIME-типы при необходимости
+      },
+    })
+  );
 };
