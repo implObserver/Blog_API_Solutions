03c10c4d77ec4738fed64dfafcb471d0bc99ce5d
implObserver <prognozman@mail.ru>
2024-10-23 22:09:39 +0500

    fix:debug cookie

--- Back_End/controllers/user/helper/middlewares/cookie/cookieController.js
+++ Back_End/controllers/user/helper/middlewares/cookie/cookieController.js
@@ -4,25 +4,31 @@ const set_cookie = asyncHandler(async (req, res, next) => {
   const user = res.locals.user;
   const refreshToken = res.locals.refreshToken;
   const acessToken = res.locals.acessToken;
+
   res.cookie('refreshToken', refreshToken, {
     httpOnly: true,
-    secure: true, // только для HTTPS
-    sameSite: 'Lax', // или 'Lax', в зависимости от вашего случая
+    secure: true,
+    sameSite: 'None',
+    maxAge: 3600000, // 1 час
   });
+
   res.cookie('acessToken', acessToken, {
     httpOnly: true,
-    secure: true, // только для HTTPS
-    sameSite: 'Lax', // или 'Lax', в зависимости от вашего случая
+    secure: true,
+    sameSite: 'None',
+    maxAge: 3600000, // 1 час
   });
-  res.cookie('user_id', user.id);
+
+  res.cookie('user_id', user.id, {
+    maxAge: 3600000, // 1 час
+  });
+
+  // Логируем куки
   console.log('Cookies set:', {
     refreshToken: refreshToken,
     acessToken: acessToken,
     user_id: user.id,
   });
+
   next();
 });
-
-export const cookieMiddlewares = {
-  set_cookie,
-};
