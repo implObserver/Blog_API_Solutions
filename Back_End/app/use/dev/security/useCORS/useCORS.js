aa0286b0cb4b5caa54a055cdd12003f8ec1403dc
implObserver <prognozman@mail.ru>
2024-10-23 20:03:45 +0500

    fix:cors

--- Back_End/app/use/dev/security/useCORS/useCORS.js
+++ Back_End/app/use/dev/security/useCORS/useCORS.js
@@ -1,21 +1,27 @@
 import cors from 'cors';
 import { app } from '../../../../app.js';
 
+// Проверка на наличие переменных окружения
+const creatorUrl =
+  process.env.CREATOR_URL || 'https://blogapifronttwo.netlify.app/'; // Значение по умолчанию
+const blogUrl = process.env.BLOG_URL || 'https://blogapifront.netlify.app/'; // Значение по умолчанию
+
 const corsOptions = {
   origin: (origin, callback) => {
-    const allowedOrigins = [process.env.CREATOR_URL, process.env.BLOG_URL];
+    const allowedOrigins = [creatorUrl, blogUrl];
     // Разрешаем запросы без origins (например, когда запрос исходит из Postman)
-    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+    if (!origin || allowedOrigins.includes(origin)) {
       callback(null, origin);
     } else {
+      console.error(`CORS error: Origin ${origin} is not allowed.`);
       callback(new Error('Not allowed by CORS'));
     }
   },
-  credentials: true, //access-control-allow-credentials:true
-  optionSuccessStatus: 200,
+  credentials: true, // access-control-allow-credentials: true
+  optionsSuccessStatus: 200,
 };
 
 export const useCORS = () => {
   app.use(cors(corsOptions));
-  app.options('*', cors(corsOptions));
+  app.options('*', cors(corsOptions)); // Обработка preflight-запросов
 };
