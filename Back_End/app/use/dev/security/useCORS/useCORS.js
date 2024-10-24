5c524b4c92f49cdd5197399e1f55a9a105eb3c92
implObserver <prognozman@mail.ru>
2024-10-24 16:52:10 +0500

    fix:CORS

--- Back_End/app/use/dev/security/useCORS/useCORS.js
+++ Back_End/app/use/dev/security/useCORS/useCORS.js
@@ -1,10 +1,9 @@
 import cors from 'cors';
-import express from 'express';
+import { app } from '../../../../app.js';
 
-const app = express();
+// Проверка на наличие переменных окружения
 
 export const useCORS = () => {
-  // Логирование информации о запросе
   app.use((req, res, next) => {
     console.log('Request Origin:', req.headers.origin); // Логируем origin
     console.log('Request Headers:', req.headers); // Логируем заголовки запроса
@@ -12,73 +11,6 @@ export const useCORS = () => {
     next();
   });
 
-  // Настройка CORS
-  app.use(
-    cors({
-      origin: [
-        'http://localhost:3000',
-        'http://localhost:5000',
-        'http://localhost:5001',
-        'https://blogapifronttwo.netlify.app',
-        'https://blogapifront.netlify.app',
-      ],
-      credentials: true,
-      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-      allowedHeaders:
-        'Origin, X-Requested-With, Content-Type, Accept, Authorization',
-    })
-  );
-
-  // Явное добавление заголовков для всех ответов
-  app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', req.headers.origin || '*');
-    res.header(
-      'Access-Control-Allow-Methods',
-      'GET,HEAD,PUT,PATCH,POST,DELETE'
-    );
-    res.header(
-      'Access-Control-Allow-Headers',
-      'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-    );
-    next();
-  });
-
-  // Обработка preflight-запросов типа OPTIONS
-  app.options('*', (req, res) => {
-    res.header('Access-Control-Allow-Origin', req.headers.origin || '*');
-    res.header(
-      'Access-Control-Allow-Methods',
-      'GET,HEAD,PUT,PATCH,POST,DELETE'
-    );
-    res.header(
-      'Access-Control-Allow-Headers',
-      'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-    );
-    res.sendStatus(200);
-  });
-
-  // Ваши маршруты должны быть подключены после CORS middleware
-};
-/*
-  export const useCORS = () => {
-  app.use((req, res, next) => {
-    console.log('Request Origin:', req.headers.origin); // Логируем origin
-    console.log('Request Headers:', req.headers); // Логируем заголовки запроса
-    console.log(`CORS middleware triggered for origin: ${req.headers.origin}`);
-    next();
-  });
-  app.use(
-    cors({
-      origin: [
-        'http://localhost:3000',
-        'http://localhost:5000',
-        'http://localhost:5001',
-        'https://blogapifronttwo.netlify.app',
-        'https://blogapifront.netlify.app',
-      ],
-      credentials: true,
-    })
-  );
   app.use((req, res, next) => {
     res.header(
       'Access-Control-Allow-Headers',
@@ -87,4 +19,3 @@ export const useCORS = () => {
     next();
   });
 };
-*/
