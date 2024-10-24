c27408b9253e11b5ad51a29fecb22f9b946a6f5f
implObserver <prognozman@mail.ru>
2024-10-24 17:31:06 +0500

    fix:logout

--- Front_End/src/entities/user/model/slice/services/thunks/auth/logout.ts
+++ Front_End/src/entities/user/model/slice/services/thunks/auth/logout.ts
@@ -4,8 +4,8 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 export const logout = createAsyncThunk(
     'services/auth/logout',
     async (thunkApi, { getState }) => {
+        await AuthService.logout();
         localStorage.clear();
         indexedDB.deleteDatabase('blog_api_creater_idb');
-        await AuthService.logout();
     }
 )
