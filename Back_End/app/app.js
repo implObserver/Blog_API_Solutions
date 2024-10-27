3ec649b70c2352ebe776519b1d3fed8253697c58
implObserver <prognozman@mail.ru>
2024-10-27 14:46:27 +0500

    feat:add cloud storage

--- Back_End/app/app.js
+++ Back_End/app/app.js
@@ -1,7 +1,6 @@
 import express from 'express';
 import { useGlobalSetters } from './set/index.js';
 import { useGlobalMiddlewares } from './use/index.js';
-import { prismaDB } from '../database/prisma/queries.js';
 //prismaDB.updateTags();
 export const app = express();
 
